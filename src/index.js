69516e2675b48ceeaa2b46266f5dfef2474c0797
Josiah Nunemaker <yoshiya.d.n@gmail.com>
2019-03-06 20:40:29 -0500

    Add particles.js background

--- src/index.js
+++ src/index.js
@@ -1,2 +1,9 @@
+import 'particles.js';
 import './style.scss';
 console.log('heyo');
+
+
+const particlesConfig = require('./assets/particles.json');
+
+
+particlesJS('particles-js', particlesConfig);
--- webpack.config.js
+++ webpack.config.js
@@ -19,6 +19,7 @@ module.exports = {
   },
   devServer: {
     contentBase: './dist',
+    host: '0.0.0.0'
   },
   module: {
     rules: [
